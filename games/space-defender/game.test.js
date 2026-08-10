35fcaa6412c4b6d7cb2b0c08afeb87046aa30955
Michael McCann <michaeljohnmccannai@gmail.com>
2026-08-10 16:18:59 +0000

    Force clients to load Space Defender thrust fix

--- games/space-defender/game.test.js
+++ games/space-defender/game.test.js
@@ -4,3 +4,7 @@ test('thrust velocity is applied to ship position every frame',()=>{
   assert.match(game,/ship\.x=C\.wrap\(ship\.x\+ship\.vx\*dt,w\)/);
   assert.match(game,/ship\.y=C\.wrap\(ship\.y\+ship\.vy\*dt,h\)/);
 });
+test('hosted page cache-busts the corrected movement script',()=>{
+  const html=fs.readFileSync(path.join(__dirname,'index.html'),'utf8');
+  assert.match(html,/game\.js\?v=1\.0\.1/);
+});
